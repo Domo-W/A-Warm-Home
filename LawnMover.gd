2bf43ef50a51d9655548284f37d1a2b6274a7a5a
VriskaSekret <148964668+VriskaSekret@users.noreply.github.com>
2024-07-19 22:02:10 +1200

    Lawn mowing started and files
    as titles suggests

---
+++ LawnMover.gd
@@ -0,0 +1,16 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+
+
+func _physics_process(delta):
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
