0c10bb5d2202e845d466d5aaefa4ad6dce3a9713
VriskaSekret <148964668+VriskaSekret@users.noreply.github.com>
2024-07-20 10:31:07 +1200

    Added temporary mowing stuff
    -Added different input types (move_left, move_right, etc.)
    -Added interact bind

--- Scripts/GeoffScripts/LawnMover.gd
+++ Scripts/GeoffScripts/LawnMover.gd
@@ -1,16 +1,27 @@
 extends CharacterBody2D
-
+@onready var sprite_2d = $Sprite2D
 
 const SPEED = 300.0
-
 
 func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var updown = Input.get_axis("move_up", "move_down")
+	var direction = Input.get_axis("move_left", "move_right")
+	
+	if direction > 0:
+		sprite_2d.flip_h = true
+	elif direction < 0:
+		sprite_2d.flip_h = false
+	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+	if updown:
+		velocity.y = updown * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+		
 
 	move_and_slide()
