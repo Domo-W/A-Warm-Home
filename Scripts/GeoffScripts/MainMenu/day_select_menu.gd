36b73d5b66004f0e234ff001fdc93e78237c3c15
VriskaSekret <148964668+VriskaSekret@users.noreply.github.com>
2024-07-21 09:23:48 +1200

    Main menu stuff

---
+++ Scripts/GeoffScripts/MainMenu/day_select_menu.gd
@@ -0,0 +1,25 @@
+extends MarginContainer
+
+
+func _on_day_1_pressed():
+	pass # Replace with function body.
+
+
+func _on_day_2_pressed():
+	pass # Replace with function body.
+
+
+func _on_day_3_pressed():
+	pass # Replace with function body.
+
+
+func _on_day_4_pressed():
+	pass # Replace with function body.
+
+
+func _on_day_5_pressed():
+	pass # Replace with function body.
+
+
+func _on_return_pressed():
+	get_tree().change_scene_to_file("res://PrototypeLevels/main_menu.tscn")
---
+++ Scripts/GeoffScripts/MainMenu/main_menu.gd
@@ -0,0 +1,12 @@
+extends MarginContainer
+
+func _on_new_game_pressed():
+	pass # Replace with function body.
+
+
+func _on_day_pick_pressed():
+	get_tree().change_scene_to_file("res://PrototypeLevels/day_select_menu.tscn")
+
+
+func _on_options_pressed():
+	pass # Replace with function body.
