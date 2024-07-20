e79dd8574bc05438937c1260705598b90d5584ca
Abhijot Malhi <33820960+tabyrocket@users.noreply.github.com>
2024-07-20 18:15:05 +1200

    Scene edits

--- Scripts/Taby Scripts/stickplayer.gd
+++ Scripts/Taby Scripts/stickplayer.gd
@@ -1,28 +1,26 @@
 extends CharacterBody2D
+@onready var sprite_2d = $AnimatedSprite2D
 
 
-const SPEED = 200.0
-const JUMP_VELOCITY = -400.0
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+const VSPEED = 100.0
+const HSPEED = 150.0
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	var updown = Input.get_axis("move_up", "move_down")
 	var direction = Input.get_axis("move_left", "move_right")
+	
+	if direction > 0:
+		sprite_2d.flip_h = true
+	elif direction < 0:
+		sprite_2d.flip_h = false
+	
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * HSPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, HSPEED)
+	if updown:
+		velocity.y = updown * VSPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, VSPEED)
 
 	move_and_slide()
