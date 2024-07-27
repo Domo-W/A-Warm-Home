9aaf43e5e997b09ded195da09bdea3d9b064b647
VriskaSekret <148964668+VriskaSekret@users.noreply.github.com>
2024-07-27 18:19:44 +1200

    Added end screen and fixed trash not resetting bug

--- Scripts/GeoffScripts/TrashManager.gd
+++ Scripts/GeoffScripts/TrashManager.gd
@@ -1,5 +1,6 @@
 extends Node
 
+@onready var end_screen = %EndScreen
 
 var rec_scene = preload("res://Scripts/GeoffScripts/draggable_recycling.tscn")
 var trash_scene = preload("res://Scripts/GeoffScripts/draggable_trash.tscn")
@@ -14,10 +15,11 @@ func _ready():
 			trash = rec_scene.instantiate()
 		trash.position = Vector2(randi_range(-150,150),randi_range(0,280))
 		add_child(trash)
+	end_screen.visible = false
 
 func throw_trash():
 	remaining_trash -= 1
 	print(str(remaining_trash))
 	if remaining_trash <= 0:
 		Global.has_done_task = true
-		get_tree().change_scene_to_file("res://PrototypeLevels/living_room.tscn")
+		end_screen.visible = true;
